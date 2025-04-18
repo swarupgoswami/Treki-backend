db841383d75e10689f5038ba1191892fa5e715ff
swarup Goswami <goswamiswarup97@gmail.com>
2025-04-18 19:52:48 +0530

    created the authMiddleware

---
+++ backend/middleware/authMidlleware.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET || "your_secret_key";
+
+exports.verifyToken = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) return res.status(401).json({ error: "Unauthorized" });
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.userId = decoded.userId;
+    next();
+  } catch (err) {
+    res.status(401).json({ error: "Invalid token" });
+  }
+};
