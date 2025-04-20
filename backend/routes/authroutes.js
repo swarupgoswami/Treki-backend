5bf316b2fc3e2b96f7baa1831b7340404895595c
swarup Goswami <goswamiswarup97@gmail.com>
2025-04-20 08:47:17 +0530

    added userDetails routes

--- backend/routes/authroutes.js
+++ backend/routes/authroutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { register, login } = require("../controllers/authController");
+const { register, login ,userDetails } = require("../controllers/authController");
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/user", userDetails);
+
 
 module.exports = router;
